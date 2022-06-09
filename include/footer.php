f0aecb99e54cf3c1c69aa7855acf41a312a8a4b8
MrKunBoy <tn757922@gmail.com>
2022-06-09 18:26:17 +0700

    upload

---
+++ include/footer.php
@@ -0,0 +1,53 @@
+<!-- footer -->
+<footer>
+		<div class="footer-top-first">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<!-- footer first section -->
+				<h2 class="footer-top-head-w3l font-weight-bold mb-2">Giới thiệu về chúng tôi :</h2>
+				<p class="footer-main mb-4">
+				Shop bán hàng được xây dựng nhằm cung cấp cho người sử dụng những trải nghiệm dễ dàng, an toàn và nhanh chóng khi mua sắm trực tuyến thông qua hệ thống hỗ trợ thanh toán và vận hành vững mạnh.<br>
+				Shop bán hàng độc quyền cung cấp chính sách mua sắm online an toàn, chỉ thanh toán cho người bán khi người mua đã nhận được hàng thành công.</p>
+				<!-- //footer first section -->
+				<!-- footer second section -->
+				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Miễn phí vận chuyển</h3>
+								<p>Đơn hàng trên 1 triệu</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Vận chuyển nhanh</h3>
+								<p>Toàn quốc</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Tin cậy</h3>
+								<p>Sự đảm bảo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
